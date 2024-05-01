a5c88ae3d3d332207d4a55900d7e4a09ae4e8f6e
anton <anton.tiunov.84.07@gmail.com>
2024-05-01 15:02:20 +0500

    7 добавил обработку исключений и логирование

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,28 @@
 import winston, { format } from 'winston';
+import * as sourceMapSupport from 'source-map-support';
+import callsite from 'callsite';
+
+sourceMapSupport.install();
+
+const internalModuleRegex = /node_modules|winston|logform/;
 
 const logger = winston.createLogger({
   level: 'info',
   format: format.combine(
     format.label({ label: 'your-service' }),
     format.timestamp(),
-    format.printf(({ level, message, label, timestamp, file, line }) => {
-      return `${timestamp} [${label}] ${file}:${line} ${level}: ${message}`;
+    format.printf(({ level, message, label, timestamp }) => {
+      const stack = callsite();
+      const callerInfo = stack.find(site => !internalModuleRegex.test(site.getFileName()));
+
+      if (callerInfo) {
+        const fileName = callerInfo.getFileName();
+        const lineNumber = callerInfo.getLineNumber();
+
+        return `${timestamp} [${label}] ${fileName}:${lineNumber} ${level}: ${message}`;
+      } else {
+        return `${timestamp} [${label}] ${level}: ${message}`;
+      }
     })
   ),
   defaultMeta: { service: 'your-service' },
